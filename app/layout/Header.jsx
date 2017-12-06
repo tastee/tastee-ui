6aec9437baa3f9aec25881f764d1ff6acc022d6b
Antoine Stalin <astalin@norsys.fr>
2017-12-06 16:36:41 +0100

    clear all

--- app/layout/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PageHeader from 'react-bootstrap/lib/PageHeader';
-import Image from 'react-bootstrap/lib/Image';
-
-export default class Header extends React.Component {
-    render() {
-        return <PageHeader>Tastee <Image id="imgTastee" src="assets/images/tastee.png" /> Ui</PageHeader>
-    }
-}
