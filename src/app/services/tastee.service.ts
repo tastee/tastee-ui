3898d8ed22db0e76dae19f2e7d2d6cabd4db8bf2
Yan MATAGNE <ymatagne@norsys.fr>
2017-12-06 12:08:17 -0500

    feat(tastee) : Create Tastee service

---
+++ src/app/services/tastee.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { TasteeCore } from 'tastee-core/transpiled/src/app/tastee-core';
+import { TasteeReporter } from 'tastee-core/transpiled/src/app/tastee-reporter';
+import { TasteeEngine } from 'tastee-core/transpiled/src/app/tastee-engine';
+import { TasteeAnalyser } from 'tastee-core/transpiled/src/app/tastee-analyser';
+
+@Injectable()
+export class TasteeService {
+
+  constructor() { }
+
+}
