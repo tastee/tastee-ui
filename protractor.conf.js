0b46509d363a536cba01d571453a3432b6429ec5
Antoine Stalin <astalin@norsys.fr>
2017-12-06 17:06:42 +0100

    feat(desktop) introduce electron

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,25 +4,33 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
+  allScriptsTimeout: 25000,
+  getPageTimeout: 15000,
+  delayBrowserTimeInSeconds: 0,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      binary: './node_modules/electron/dist/electron.exe',
+      args: ['--test-type=webdriver', 'app=dist/main.js']
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
+  framework: 'jasmine2',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
-  onPrepare() {
+  beforeLaunch: function () {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
+  },
+  onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
