14179a21283b7045c919c8a37cb3f47e9d32657a
FrancoisBs <francois.boissier@gmail.com>
2017-12-12 19:39:04 +0100

    fix(import) : clean import of tastee modules

--- src/app/services/tastee.service.ts
+++ src/app/services/tastee.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import { TasteeCore } from 'tastee-core/transpiled/src/app/tastee-core';
-import { TasteeReporter } from 'tastee-core/transpiled/src/app/tastee-reporter';
-import { TasteeEngine } from 'tastee-core/transpiled/src/app/tastee-engine';
-import { TasteeAnalyser } from 'tastee-core/transpiled/src/app/tastee-analyser';
+import { TasteeCore } from 'tastee-core';
+import { TasteeReporter } from 'tastee-core';
+import { TasteeEngine } from 'tastee-core';
+import { TasteeAnalyser } from 'tastee-core';
 import { ExtractTasteeCode } from 'tastee-html';
 import { Workspace } from 'app/models/workspace';
 import { FileService } from 'app/services/file.service';
